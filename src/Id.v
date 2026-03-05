8cd812b6441d46d4dc4b80a0cf56b39191a7553e
Maxim Rodionov <rodionov.maxim05@gmail.com>
2026-03-05 03:02:26 +0300

    feat: prove Id.v

--- src/Id.v
+++ src/Id.v
@@ -64,30 +64,63 @@ Lemma le_gt_id_dec : forall id1 id2 : id, {id1 i<= id2} + {id1 i> id2}.
 Proof. prove_with le_gt_dec. Qed.
 
 Lemma id_eq_dec : forall id1 id2 : id, {id1 = id2} + {id1 <> id2}.
-Proof. admit. Admitted.
+Proof. prove_with Nat.eq_dec. Qed.
 
 Lemma eq_id : forall (T:Type) x (p q:T), (if id_eq_dec x x then p else q) = p.
-Proof. admit. Admitted.
+Proof.
+  intros. destruct (id_eq_dec x x).
+  - reflexivity.
+  - contradiction.
+Qed.
 
 Lemma neq_id : forall (T:Type) x y (p q:T), x <> y -> (if id_eq_dec x y then p else q) = q.
-Proof. admit. Admitted.
+Proof.
+  intros. destruct (id_eq_dec x y).
+  - contradiction.
+  - reflexivity.
+Qed.
 
 Lemma lt_gt_id_false : forall id1 id2 : id,
     id1 i> id2 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros [n] [m] H1 H2.
+  inversion H1. inversion H2. subst.
+  lia.
+Qed.
 
 Lemma le_gt_id_false : forall id1 id2 : id,
     id2 i<= id1 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros [n] [m] H1 H2.
+  inversion H1. inversion H2. subst.
+  lia.
+Qed.
 
 Lemma le_lt_eq_id_dec : forall id1 id2 : id, 
     id1 i<= id2 -> {id1 = id2} + {id2 i> id1}.
-Proof. admit. Admitted.
+Proof.
+  intros [n] [m] H.
+  destruct (Nat.eq_dec n m) as [Heq | Hne].
+  - left. subst. reflexivity.
+  - right. constructor.
+    inversion H. subst.
+    lia.
+Qed.
 
 Lemma neq_lt_gt_id_dec : forall id1 id2 : id,
     id1 <> id2 -> {id1 i> id2} + {id2 i> id1}.
-Proof. admit. Admitted.
-    
+Proof.
+  intros.
+  destruct (gt_eq_gt_id_dec id1 id2) as [[Hgt | Heq] | Hlt].
+  - left. apply Hgt.
+  - contradiction.
+  - right. apply Hlt.
+Qed.
+
 Lemma eq_gt_id_false : forall id1 id2 : id,
     id1 = id2 -> id1 i> id2 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros [n] [m] H1 H2.
+  inversion H1. inversion H2. subst.
+  lia.
+Qed.
